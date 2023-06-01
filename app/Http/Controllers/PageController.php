311846f519079b68c9a18e26d3c8710ed97041d6
NaiDang <Naid4243@gmail.com>
2023-06-01 19:13:36 +0700

    commit

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,7 +12,6 @@ class PageController extends Controller
 {
     public function getIndex(){
         $slide=Slide::all();
-        // return view('page.trangchu');
         $new_product = Product::where('new',1)->paginate(8);
         $sanpham_khuyenmai=	Product::where('promotion_price','<>',0)->paginate(4);
         return view('Page.trangchu',compact('slide','new_product','sanpham_khuyenmai'));
