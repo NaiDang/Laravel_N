012fe355f2e2e112ea0367b0e5cf19a2193e4331
NaiDang <Naid4243@gmail.com>
2023-06-01 18:17:28 +0700

    new

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,22 +1,40 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Models\Slide;
 use Illuminate\Http\Request;
+use App\Models\Slide;
+use App\Models\Product;
+use App\Models\Product_type;
+use App\Models\Comment;
 
 class PageController extends Controller
 {
-    public function getIndex()
-    {
-        $slide = Slide::all();
-        return view('Page.trangchu', compact('slide'));
-        $new_product=product::where('new',1)->get();
+    public function getIndex(){
+        $slide=Slide::all();
+        // return view('page.trangchu');
+        $new_product = Product::where('new',1)->paginate(8);
+        $sanpham_khuyenmai=	Product::where('promotion_price','<>',0)->paginate(4);
+        return view('Page.trangchu',compact('slide','new_product','sanpham_khuyenmai'));
+       
+    }
+    public function getDetail(Request $request){
+        $sanpham=Product::where('id',$request->id)->first();
+        $splienquan=Product::where('id','<>',$sanpham->id,'and','id_type','=',$sanpham->id_type)->paginate(3);
+        $comment=Comment::where('id_product',$request->id)->first();
+        return view('Page.chitietsanpham',compact('sanpham','splienquan','comment'));
     }
 
-    public function getLoaiSp()
+    public function getLoaiSp($type)
     {
-        return view('Page.loaisanpham');
+        
+        $sp_theoloai=Product::where('id_type',$type)->get();
+        $sp_khac=Product::where('id_type','<>',$type)->paginate(3);
+
+        $loai=Product_type::all();
+        $loai_sp=Product_type::where('id',$type)->first();
+
+        return view('Page.loaisanpham',compact('sp_theoloai','sp_khac','loai','loai_sp'));
     }
 
     public function getChitiet()
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,34 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Models\Product;
+use App\Models\Product_type;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        view()->composer('header',function($view){
+            $loai_sp=Product_type::all();
+            $view->with('loai_sp',$loai_sp);
+        });
+        // view()->composer('page.loai_sanpham',function($view){
+        //     $loai_sp=Type_product::all();
+        //     $view->with('loai_sp',$loai_sp);
+        // });
+        view()->composer('page.product_type',function($view){
+            $prouct_new=Product_type::where('new',1)->orderBy('id','DESC')->skip(1)->take(8)->get();
+            $view->with('product_new',$prouct_new);
+        });
     }
 }
